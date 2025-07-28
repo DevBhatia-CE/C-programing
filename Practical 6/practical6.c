1de2d25e25ed36856a5eebec3fdca4f188cd90eb
DevBhatia-CE <devbhatia.ce@gmail.com>
2025-07-28 15:28:17 +0530

    Add files via upload

---
+++ Practical 6/practical6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float fahrenheit , celsius;
+printf("Enter the Value of Temprature(In celsius):");
+scanf("%f",&celsius);
+fahrenheit = (celsius*9/5) + 32 ;
+printf("\nThe value of temprature you entered in Fahrenheit is %f F",fahrenheit);
+printf("\nThis practical is performed by Dev Bhatia-25TCEOLE");
+return 0;
+}
